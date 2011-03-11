2eaac903626feb20144e25625bdc8d56e6fbde14
Spencer Jackson <spencerandrewjackson@gmail.com>
2011-03-11 18:40:53 -0500

    Whitespace and comment fixup

--- main.cpp
+++ main.cpp
@@ -3,11 +3,14 @@
 using namespace cv;
 
 int main(int argc, char* argv[]) {
+	//Init camera feed and GUI output
 	VideoCapture capture(0);
 	if (!capture.isOpened()) return -1;
 	namedWindow("display", 1);
 	Mat frame;
-        while (true) {
+
+	//Loop outputing collected data
+	while (true) {
 		capture >> frame;
 		imshow("display", frame);
 		if (waitKey(30) >= 0 ) break;
