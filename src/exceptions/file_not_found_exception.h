02180c639bf0358869fcd97ef3e50269378f6fa2
Nohemi Fernandez <nf68@cornell.edu>
2011-03-11 21:18:46 -0500

    Create file not found exception

---
+++ src/exceptions/file_not_found_exception.h
@@ -0,0 +1,14 @@
+#include <exception>
+#include <string>
+using namespace std;
+
+class FileNotFoundException : public exception {
+public:
+	FileNotFoundException(string const& file) throw() {this->file = file;};
+	virtual ~FileNotFoundException() throw() {};
+	virtual const char* what() const throw() {
+		return string("File not found").c_str();
+	};
+protected:
+	string file;
+};
