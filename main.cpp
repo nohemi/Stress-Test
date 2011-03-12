1db58a4cc68659a0688e3f5a86cd22b2a05b79d3
Nohemi Fernandez <nf68@cornell.edu>
2011-03-12 01:51:53 -0500

    Hough circles for entire frame

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char* argv[]) {
 	VideoCapture capture(0);
 	if (!capture.isOpened()) return -1;
 	namedWindow("display", 1);
-	Mat frame;
+	Mat frame, gray;
 
 	MetricContainer container;
 
@@ -22,12 +22,19 @@ int main(int argc, char* argv[]) {
 		vector<Rect> eye_frame;
 		eye_frame = eyes.detectEyes(frame);
 		for (vector<Rect>::iterator it = eye_frame.begin(); it != eye_frame.end(); ++it) {
-			//cout << it->x << endl;
-			//cout << it->y << endl;
 			Point pt1(it->x,it->y);
 			Point pt2(it->x + it->width,it->y + it->height);
 			rectangle(frame,pt1,pt2,Scalar(0,0,255));
 		}
+
+		vector<Vec3f> circles;
+		cvtColor(frame, gray, CV_BGR2GRAY);
+		HoughCircles(gray, circles, CV_HOUGH_GRADIENT, 2, gray.rows/50, 50, 200 );
+		circle(gray, cvPoint(cvRound(circle[0]),cvRound(circle[1])),cvRound(circle[2]),CV_RGB(255,0,0),-1);*/
+		for (vector<Vec<float, 3> >::iterator it = circles.begin(); it != circles.end(); it++) {
+			circle(frame, cvPoint((*it)[0], (*it)[1]), (*it)[2], CV_RGB(255,0,0),1);
+		}
+
 		imshow("display", frame);
 		if (waitKey(30) >= 0 ) break;
 	}
