9de0642a616ab8535c72a8a970728911f48c95be
Nohemi Fernandez <nf68@cornell.edu>
2011-03-12 07:00:40 -0500

    Add extra XML for left eye split

--- src/finder.cpp
+++ src/finder.cpp
@@ -10,7 +10,8 @@ vector<Rect> Finder::detectEyes (const Mat& image) {
 	//Detect eyes
 	CascadeClassifier eyes;
 	vector<Rect> objects;
-	string filename("resources/haarcascade_mcs_eyepair_big.xml");
+	//string filename("resources/haarcascade_mcs_eyepair_big.xml");
+	string filename("/usr/share/opencv/haarcascades/haarcascade_lefteye_2splits.xml");
 	if (!eyes.load(filename)) throw FileNotFoundException(filename);
 	eyes.detectMultiScale(image,objects);
 	return objects;
