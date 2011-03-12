3ed4a43fd1409cbeb789846e3ca8efbd7bd0f2b6
Nohemi Fernandez <nf68@cornell.edu>
2011-03-11 22:56:25 -0500

    Modified broken xml link

--- src/finder.cpp
+++ src/finder.cpp
@@ -1,6 +1,7 @@
 #include "finder.h"
 #include "exceptions/file_not_found_exception.h"
 #include "cv.h"
+#include <iostream>
 
 using namespace cv;
 using namespace std;
@@ -9,7 +10,7 @@ vector<Rect> Finder::detectEyes (const Mat& image) {
 	//Detect eyes
 	CascadeClassifier eyes;
 	vector<Rect> objects;
-	const string filename("haarcascade_mcs_eyepair_big.xml");
+	string filename("resources/haarcascade_mcs_eyepair_big.xml");
 	if (!eyes.load(filename)) throw FileNotFoundException(filename);
 	eyes.detectMultiScale(image,objects);
 	return objects;
